dd99812f5899880d331bb6d6902d508304dc2a5e
Krzysztof Kurzydło <krzysztof.kurzydlo@creativestyle.pl>
2018-09-14 11:21:45 +0200

    Introduced block for displaying icons

---
+++ Block/Icon.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MageSuite\ThemeHelpers\Block;
+
+class Icon extends \Magento\Framework\View\Element\Template
+{
+    protected $_template = 'MageSuite_ThemeHelpers::icon.phtml';
+
+    public function getIconUrl() {
+        $url = $this->getData('icon_url');
+
+        if(substr($url, 0, 4) == 'http') {
+            return $url;
+        }
+
+        return $this->getViewFileUrl($url);
+    }
+}
