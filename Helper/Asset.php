e52002e698b24384f8909f5b64a71410817005c6
Dariusz Twardowski <dariusz.twardowski@creativestyle.pl>
2023-07-30 11:46:15 +0200

    fix: [DOG-1689] Error log dogma theme

--- Helper/Asset.php
+++ Helper/Asset.php
@@ -1,20 +1,20 @@
 <?php
 
+declare(strict_types=1);
 namespace MageSuite\ThemeHelpers\Helper;
 
 class Asset extends \Magento\Framework\App\Helper\AbstractHelper
 {
-    /**
-     * @var \Magento\Framework\View\Asset\Repository
-     */
-    protected $assetRepository;
+    protected \Magento\Framework\View\Asset\Repository $assetRepository;
 
-    public function __construct(\Magento\Framework\View\Asset\Repository $assetRepository)
-    {
+    public function __construct(
+        \Magento\Framework\View\Asset\Repository $assetRepository
+    ) {
         $this->assetRepository = $assetRepository;
     }
 
-    public function getViewFileContents($assetPath) {
+    public function getViewFileContents($assetPath):string
+    {
         $asset = $this->assetRepository->createAsset($assetPath);
 
         return $asset->getContent();
