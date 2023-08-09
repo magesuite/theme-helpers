08256214d4c36703285f0c1f75fb9897df5ba5b5
Dariusz Twardowski <dariusz.twardowski@creativestyle.pl>
2023-08-09 20:59:00 +0000

    Update AssetTest.php

--- Test/Integration/Helper/AssetTest.php
+++ Test/Integration/Helper/AssetTest.php
@@ -17,7 +17,7 @@ class AssetTest extends \PHPUnit\Framework\TestCase
 
     public function testItReturnsContentOfAnAsset():void
     {
-        $this->assertContains('asset_contents', $this->assetHelper->getViewFileContents('MageSuite_ThemeHelpers::images/asset.txt'));
+        $this->assertStringContainsString('asset_contents', $this->assetHelper->getViewFileContents('MageSuite_ThemeHelpers::images/asset.txt'));
     }
 
     public function testItReturnsNullWhenAssetDoesNotExist():void
