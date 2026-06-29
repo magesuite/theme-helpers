b20b7dc0558ef7e0c8f960bf03ce83259055f457
Zbigniew Kuraś <zbigniew.kuras@creativestyle.pl>
2026-06-29 07:41:48 +0200

    feat: MGS-5268 remove webp features

--- Test/Unit/Helper/ImageTypeTest.php
+++ Test/Unit/Helper/ImageTypeTest.php
@@ -9,40 +9,11 @@ class ImageTypeTest extends \PHPUnit\Framework\TestCase
     protected ?\Magento\TestFramework\ObjectManager $objectManager;
     protected ?\MageSuite\ThemeHelpers\Helper\ImageType $imageType;
 
-    /**
-     * @var \Magento\Framework\App\Config\ScopeConfigInterface
-     */
-    protected ?\PHPUnit\Framework\MockObject\MockObject $scopeConfig;
-
     public function setUp(): void
     {
         $this->objectManager = \Magento\TestFramework\ObjectManager::getInstance();
 
-        $this->scopeConfig = $this->getMockBuilder(\Magento\Framework\App\Config\ScopeConfigInterface::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $this->scopeConfig->method('getValue')->willReturn(true);
-
-        $this->imageType = $this->objectManager->create(\MageSuite\ThemeHelpers\Helper\ImageType::class, ['scopeConfig' => $this->scopeConfig]);
-    }
-
-    #[\PHPUnit\Framework\Attributes\DataProvider('getWebpUrls')]
-    public function testItResolvesWebpSupport(string $path, bool $expectedResult): void
-    {
-        $this->assertEquals($expectedResult, $this->imageType->supportsWebp($path));
-    }
-
-    public static function getWebpUrls(): array
-    {
-        return [
-            ['/var/images/image.jpg', true],
-            ['https://www.example.com/image.jpg', true],
-            ['https://www.example.com/image.jpeg', true],
-            ['https://www.example.com/image.png', true],
-            ['https://www.example.com/image.PNG', true],
-            ['https://www.example.com/image.gif', false],
-        ];
+        $this->imageType = $this->objectManager->create(\MageSuite\ThemeHelpers\Helper\ImageType::class);
     }
 
     #[\PHPUnit\Framework\Attributes\DataProvider('getMimeTypeUrls')]
